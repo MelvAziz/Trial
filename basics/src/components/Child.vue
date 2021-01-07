4aca81ac64f8a30ca34b188475c7ee12620cb674
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2021-01-07 10:37:35 -0500

    commit

--- basics/src/components/Child.vue
+++ basics/src/components/Child.vue
@@ -1,39 +1,76 @@
 <template>
-  <div>
-    <h1></h1>
-    <ul class="Quiz-Questions">
-      <li class="Quiz-item">{{ flavor }}</li>
-    </ul>
+  <div class="container">
+    <h1>Welcome to this Quiz Game!</h1>
+    <h1 v-if="started"> {{ quiz[QuestionN].question }}</h1>
+    <button v-if="started === true && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].correct_answer }} </button>
+    <button v-else-if="started === true"> {{ quiz[QuestionN].correct_answer }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[0] }} </button>
+    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[0] }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[1] }} </button>
+    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[1] }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[2] }} </button>
+    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[2] }} </button>
+    <button v-on:click="startedQuiz" v-else>Begin</button>
+    <button v-on:click="nextQuestion" v-if="submit">Submit</button>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
 export default {
-  name: "Child",
+  name: "Quiz",
+  components: {},
   data() {
-    return { 
-      question: {},
-      choices: [],
+    return {
+      quiz: [],
+      QuestionN: 0,
+      started: false,
+      submit: false,
+      clickable: false,
     };
   },
-  mounted: function() {
-    this.fetchData();
-  },
   methods: {
+    startedQuiz: function() {
+      this.started = true;
+      this.clickable = true;
+    },
+    checkAnswer: function() {
+      this.submit = true;
+      this.clickable = false;
+    },
+    nextQuestion: function () {
+      this.submit = false;
+      this.clickable = true;
+      this.QuestionN ++;
+    },
     fetchData: async function() {
       try {
         const result = await fetch (`https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple`);
         const data = await result.json();
         console.log(data);
-        this.question = data.results.question;
-        this.choices = [data.results.correct_answer, data.results.incorrect_answer[0], data.results.incorrect_answer[1], data.results.incorrect_answer[2]];
+        this.quiz = data.results;    
       }
       catch(error) {
         alert(error);
       }
     },
+    
   },
+  created() {
+    
+  },
+  mounted() {  
+        this.fetchData();
+  },
+  beforeCreate() {},
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 3rem;
+}
+button {
+  font-size: 2rem;
+}
+</style>
--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -2,14 +2,14 @@
   <div class="container">
     <h1>Welcome to this Quiz Game!</h1>
     <h1 v-if="started"> {{ quiz[QuestionN].question }}</h1>
-    <button v-if="started === true && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].correct_answer }} </button>
-    <button v-else-if="started === true"> {{ quiz[QuestionN].correct_answer }} </button>
-    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[0] }} </button>
-    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[0] }} </button>
-    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[1] }} </button>
-    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[1] }} </button>
-    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ quiz[QuestionN].incorrect_answers[2] }} </button>
-    <button v-else-if="started === true"> {{ quiz[QuestionN].incorrect_answers[2] }} </button>
+    <button v-if="started === true && clickable === true" v-on:click="checkAnswer"> {{ choices[0] }} </button>
+    <button v-else-if="started === true"> {{ choices[0] }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ choices[1] }} </button>
+    <button v-else-if="started === true"> {{ choices[1] }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ choices[2] }} </button>
+    <button v-else-if="started === true"> {{ choices[2] }} </button>
+    <button v-if="started && clickable === true" v-on:click="checkAnswer"> {{ choices[3] }} </button>
+    <button v-else-if="started === true"> {{ choices[3] }} </button>
     <button v-on:click="startedQuiz" v-else>Begin</button>
     <button v-on:click="nextQuestion" v-if="submit">Submit</button>
   </div>
@@ -18,11 +18,13 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: "Home",
-  components: {},
   data() {
     return {
       quiz: [],
+      choices: [],
+      correct: {},
+      correct_answer: {},
+      last_correct_choice: {},
       QuestionN: 0,
       started: false,
       submit: false,
@@ -33,22 +35,38 @@ export default {
     startedQuiz: function() {
       this.started = true;
       this.clickable = true;
+      this.correct_answer = 0;
+      this.correct = this.quiz[this.QuestionN].correct_answer;
+      this.choices = [this.correct, this.quiz[this.QuestionN].incorrect_answers[0], this.quiz[this.QuestionN].incorrect_answers[1], this.quiz[this.QuestionN].incorrect_answers[2]];
     },
     checkAnswer: function() {
+      this.last_correct_choice = this.correct_answer;
       this.submit = true;
       this.clickable = false;
+    },
+    shuffle: function (array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        var j = (Math.round(Math.random() * 3));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
     },
     nextQuestion: function () {
       this.submit = false;
       this.clickable = true;
       this.QuestionN ++;
+      this.correct = this.quiz[this.QuestionN].correct_answer;
+      this.choices = [this.correct, this.quiz[this.QuestionN].incorrect_answers[0], this.quiz[this.QuestionN].incorrect_answers[1], this.quiz[this.QuestionN].incorrect_answers[2]];
+      this.shuffle(this.choices);
+      console.log(this.quiz[this.QuestionN].correct_answer); 
     },
     fetchData: async function() {
       try {
         const result = await fetch (`https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple`);
         const data = await result.json();
         console.log(data);
-        this.quiz = data.results;
+        this.quiz = data.results;    
       }
       catch(error) {
         alert(error);
